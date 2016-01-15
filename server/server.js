c21ddae5e93da730a2ca58f9be7669195b4ffffb
Festus Kiprop <fyangani@gmail.com>
2016-01-15 15:09:58 -0600

    Added logger

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
+'use strict';
+
 var express    = require('express');      
 var app        = express();            
 var bodyParser = require('body-parser');
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
+var React = require('react');
 var port = process.env.PORT || 4000;  
+
+
+//serving static files
+app.configure(function () {
+    app.use("/", express.static(__dirname + '/../client/'));
+});
+
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+app.listen(port);
+console.log('Server Running on ' + port);
