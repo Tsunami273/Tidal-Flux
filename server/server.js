ff6e7afa623de47dc266b94b1a3134234a9220e8
Festus Kiprop <fyangani@gmail.com>
2016-01-15 15:17:54 -0600

    Static server running

--- server/server.js
+++ server/server.js
@@ -3,18 +3,11 @@
 var express    = require('express');      
 var app        = express();            
 var bodyParser = require('body-parser');
-var React = require('react');
-var port = process.env.PORT || 4000;  
+var React      = require('react');
+var port       = process.env.PORT || 4000;  
 
 
-//serving static files
-app.configure(function () {
-    app.use("/", express.static(__dirname + '/../client/'));
-});
-
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../'));
 
 app.listen(port);
 console.log('Server Running on ' + port);
