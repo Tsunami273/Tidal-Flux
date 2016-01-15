5512ac6efe504139774be451b6b57279c696d979
Michael Wheatley <michaelvwheatley@gmail.com>
2016-01-15 15:55:38 -0600

    sign up validation

--- client/components/signup/index.js
+++ client/components/signup/index.js
@@ -9,7 +9,8 @@ Signup = React.createClass({
       confirmPassword: '',
       emailError: '',
       usernameError: '',
-      passwordError: ''
+      passwordError: '',
+      hide:'hidden'
       };
     },
     validateEmail: function(event) {
@@ -17,6 +18,7 @@ Signup = React.createClass({
       var emailError = '';
       if(!email.includes('@')){
         this.setState({emailError: 'not a valid email'});
+        this.setState({hide:'hidden'});
       }
       else{
         this.setState({emailError: ''});
@@ -28,9 +30,11 @@ Signup = React.createClass({
       var usernameError = '';
       if(username.length <= 4){
         this.setState({usernameError: 'Username must be at least 4 characters'});
+        this.setState({hide:'hidden'});
       }
       else if(username.length > 16){
         this.setState({usernameError: 'Username must be less than 16 characters'});
+        this.setState({hide:'hidden'});
       }
       else{
         this.setState({usernameError: ''});
@@ -43,9 +47,13 @@ Signup = React.createClass({
     validatePassword: function(event){
       if(this.state.password !== event.target.value){
         this.setState({passwordError:'passwords do not match'});
+        this.setState({hide:'hidden'});
       }
       else{
         this.setState({passwordError: ''});
+        if(this.state.usernameError === '' && this.state.emailError === ''){
+          this.setState({hide:''});
+        }
       }
       this.setState({confirmPassword: event.target.value});
     },
@@ -53,6 +61,7 @@ Signup = React.createClass({
       var emailError = this.state.emailError ? 'Error, ' + this.state.emailError : '';
       var usernameError = this.state.usernameError ? 'Error, ' + this.state.usernameError : '';
       var passwordError = this.state.passwordError ? 'Error, ' + this.state.passwordError : '';
+      var hide = this.state.hide;
         return (
         <div>
           <h1>Sign Up Page</h1>
@@ -70,6 +79,7 @@ Signup = React.createClass({
             Confirm Password: <input type="password" value={this.state.confirmPassword} onChange={this.validatePassword}/>
             <br />
             {passwordError}
+            <input type="submit" className={hide}/>
           </form>
         </div>
         );
