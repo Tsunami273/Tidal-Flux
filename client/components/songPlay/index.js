1c309de8fc1e10466043a3421e4d0d6ecd3090f5
quadstar <icberg27@gmail.com>
2016-01-15 19:53:40 -0600

    song plays on load and goes to score screen on end.

--- client/components/songPlay/index.js
+++ client/components/songPlay/index.js
@@ -1,16 +1,22 @@
 // import child components here.
-var NavButton = require('../navButton.js');
- 
 SongPlay = React.createClass({
     play: function(event) {
       store.dispatch(navigateToPage('SCORE'));
     },
+    loadedSong: function(event){
+      // start progress timer.
+      this.refs.audio.play();
+    },
     render: function() {
+      var audioSource = store.getState().selectedSong;
         return (
         <div>
           <h1>Song Play</h1>
-          <br />
-          <NavButton dest="Finished" onClick={this.play} />
+          <audio controls src={'./songs/' + audioSource.id + '.ogg'} 
+          onCanPlay={this.loadedSong} 
+          onEnded={this.play}
+          ref='audio'
+          ></audio>
         </div>
         );
     }
