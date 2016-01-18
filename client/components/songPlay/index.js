6ce1d4531f6077367e1fae09af0809c7cc854127
Michael Wheatley <michaelvwheatley@gmail.com>
2016-01-18 15:42:56 -0600

    function to calculate measure start time

--- client/components/songPlay/index.js
+++ client/components/songPlay/index.js
@@ -1,5 +1,6 @@
 // import child components here.
 var beatMaps = require('./maps/');
+var findMeasureStartTimes = require('findMeasureStartTimes.js');
 
 SongPlay = React.createClass({
     getInitialState: function() {
