cf3338bca49a3d57ec6524d543b50f5417d9fae3
Festus Kiprop <fyangani@gmail.com>
2016-01-20 15:08:40 -0600

    Added bcrypt

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,11 +1,14 @@
 'use strict';
-var mongoose = require('mongoose');
 mongoose.connect('mongodb://tidal:tidal@ds047355.mongolab.com:47355/tidal');
+
+var mongoose = require('mongoose');
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
+var SALT_WORK_FACTOR = 10;
 
  
-var playerSchema = new Schema({
+var PlayerSchema = new Schema({
 	username   : { type: String, required: true, unique: true },
 	email      : { type: String, required: true, unique: true },
 	password   : { type: String, required: true },
@@ -15,5 +18,39 @@ var playerSchema = new Schema({
     avatar	   : String
 });
 
+PlayerSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
+
+
+
+
+////////////////////////////////////////////////////
 var Player = mongoose.model('Player', playerSchema);
 module.exports.Player = Player;
