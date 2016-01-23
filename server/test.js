26e0b0bd0f1ba7b564a51a939c622b81bf437f7c
Festus Kiprop <fyangani@gmail.com>
2016-01-23 10:24:24 -0600

    tests

--- server/test.js
+++ server/test.js
@@ -8,9 +8,9 @@ var server = supertest.agent("http://localhost:4000");
 
 describe("Server unit tests",function(){
   var player = {
-    "username": "iassna",
-    "email": "ianss@emassjil.com",  
-    "password": "1sssm23sdf"
+    "username": "iasssddasdsa",
+    "email": "bcryasdsapt@emassjil.com",  
+    "password": "1sssm2s3sdf"
   };
 
   var wrongPasswordPlayer = {
@@ -35,7 +35,6 @@ describe("Server unit tests",function(){
     .set('Content-type', 'application/json')
     .expect(200)
     .end(function(err,res){
-      console.log('Singup Respnose: ', res.body)
       if (err) {
         throw error;
       }
@@ -44,33 +43,33 @@ describe("Server unit tests",function(){
   });
 
 
-  // it("should be able to sign in",function(done){
-  //   server
-  //   .post('/api/player/signin')
-  //   .send(JSON.stringify(player))
-  //   .set('Content-type', 'application/json')
-  //   .end(function(err,res){
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     expect(res.status).to.equal(200);
-  //     done();
-  //   });
-  // });
+  it("should be able to sign in",function(done){
+    server
+    .post('/api/player/signin')
+    .send(JSON.stringify(player))
+    .set('Content-type', 'application/json')
+    .end(function(err,res){
+      if (err) {
+        throw err;
+      }
+      expect(res.status).to.equal(200);
+      done();
+    });
+  });
 
-  // it("should NOT BE ABLE to sign in with wrong username/password",function(done){
-  //   server
-  //   .post('/api/player/signin')
-  //   .send(JSON.stringify(wrongPasswordPlayer))
-  //   .set('Content-type', 'application/json')
-  //   .end(function(err,res){
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     expect(res.status).to.equal(403);
-  //     done();
-  //   });
-  // });
+  it("should NOT BE ABLE to sign in with wrong username/password",function(done){
+    server
+    .post('/api/player/signin')
+    .send(JSON.stringify(wrongPasswordPlayer))
+    .set('Content-type', 'application/json')
+    .end(function(err,res){
+      if (err) {
+        throw err;
+      }
+      expect(res.status).to.equal(403);
+      done();
+    });
+  });
 
   it("should return Error 404 for wrong url links",function(done){
     server
