6286e187aab35c511e03bbd42fe6942d94b05efe
quadstar <icberg27@gmail.com>
2016-01-24 13:38:15 -0600

    new beatmap and conversion utility.

---
+++ utils/smToTF.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+function smToTF(measure){
+  var result = measure.map(function(e,i,c){
+    return parseInt(e.slice(0,-2), 2);
+  });
+  return {notes: result, startTime: 0};
+}
+
+fs.readFile('./smtest.txt', 'utf8', function(err, data){
+  var measures = data.split(',');
+  var notes = measures.map(function(e,i,c){
+    return e.split('\n');
+  });
+  for(var i = 0 ; i < notes.length ; i++){
+    for(var k = 0 ; k < notes[i].length; k++){
+      if(notes[i][k] === '' || notes[i][k] === ';') notes[i].splice(k,1);
+    }
+  }
+  notes[notes.length-1].splice(notes[notes.length-1].length-1, 1);
+  measures = [];
+  for(var i = 0 ; i < notes.length ; i++){
+    measures.push(smToTF(notes[i]));
+  }
+  return console.log(measures);
+})
+
