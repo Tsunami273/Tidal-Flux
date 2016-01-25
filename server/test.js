128c5f4095ab626ec00d14120897268dd49cba02
Festus Kiprop <fyangani@gmail.com>
2016-01-24 19:52:05 -0600

    Added tests to test score functionality, passing all tests

--- server/test.js
+++ server/test.js
@@ -7,23 +7,6 @@ var server = supertest.agent("http://localhost:4000");
 
 
 describe("Server unit tests",function(){
-  var player = {
-    "username": "iasssddasdsa",
-    "email": "bcryasdsapt@emassjil.com",  
-    "password": "1sssm2s3sdf"
-  };
-
-  var scores = {
-    "username": "Mithril",
-    "song": "dog",
-    "points": "50",
-    "difficulty": "Hard"
-  }
-
-  var wrongPasswordPlayer = {
-    "username": "dfsfsdsfdsfsd",
-    "password": "sdsdsxhhxd"
-  }
 
   it("should return homepage",function(done){
     server
@@ -35,74 +18,75 @@ describe("Server unit tests",function(){
     });
   });
 
-  // it("should be able to sign up",function(done){
-  //   server
-  //   .post('/api/player/signup')
-  //   .send(JSON.stringify(player))
-  //   .set('Content-type', 'application/json')
-  //   .expect(200)
-  //   .end(function(err,res){
-  //     if (err) {
-  //       throw error;
-  //     }
-  //     done();
-  //   });
-  // });
+  it("should be able to sign up",function(done){
+    var player = {
+      "username": "iasssda",
+      "email": "bcryasdsapt@emassjil.com",  
+      "password": "1sssm2s3sdf"
+    };
+
+    server
+    .post('/api/player/signup')
+    .send(JSON.stringify(player))
+    .set('Content-type', 'application/json')
+    .expect(200)
+    .end(function(err,res){
+      if (err) {
+        throw error;
+      }
+      done();
+    });
+  });
 
 
-  // it("should be able to sign in",function(done){
-  //   server
-  //   .post('/api/player/signin')
-  //   .send(JSON.stringify(player))
-  //   .set('Content-type', 'application/json')
-  //   .end(function(err,res){
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     expect(res.status).to.equal(200);
-  //     done();
-  //   });
-  // });
+  it("should be able to sign in",function(done){
+    var player = {
+      "username": "iasssda",
+      "email": "bcryasdsapt@emassjil.com",  
+      "password": "1sssm2s3sdf"
+    };
 
-  // it("should NOT BE ABLE to sign in with wrong username/password",function(done){
-  //   server
-  //   .post('/api/player/signin')
-  //   .send(JSON.stringify(wrongPasswordPlayer))
-  //   .set('Content-type', 'application/json')
-  //   .end(function(err,res){
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     expect(res.status).to.equal(403);
-  //     done();
-  //   });
-  // });
+    server
+    .post('/api/player/signin')
+    .send(JSON.stringify(player))
+    .set('Content-type', 'application/json')
+    .end(function(err,res){
+      if (err) {
+        throw err;
+      }
+      expect(res.status).to.equal(200);
+      done();
+    });
+  });
 
-  // it("should return Error 404 for wrong url links",function(done){
-  //   server
-  //   .get("/random")
-  //   .expect(404)
-  //   .end(function(err,res){
-  //     expect(res.status).to.equal(404);
-  //     done();
-  //   });
-  // });
+  it("should NOT BE ABLE to sign in with wrong username/password",function(done){
+    var player = {
+      "username": "iasssda",
+      "password": "1ssss3sdf"
+    };
 
-  // it("Should be able to add a New Score",function(done){
-  //   server
-  //   .post('/api/player/score')
-  //   .send(JSON.stringify(scores))
-  //   .set('Content-type', 'application/json')
-  //   .end(function(err,res){
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     expect(res.status).to.equal(200);
-  //     done();
-  //   });
-  // });
+    server
+    .post('/api/player/signin')
+    .send(JSON.stringify(player))
+    .set('Content-type', 'application/json')
+    .end(function(err,res){
+      if (err) {
+        throw err;
+      }
+      expect(res.status).to.equal(403);
+      done();
+    });
+  });
 
-  it("Should add new score, update new high score or notify low score",function(done){
+
+  it("Should be able to add a new score(Including first time playing song, level)",function(done){
+    var scores = {
+      "username": "Juice",
+      "song": "dog",
+      "points": "5",
+      "difficulty": "Hard"
+    }
+
     server
     .post('/api/player/score')
     .send(JSON.stringify(scores))
@@ -112,6 +96,60 @@ describe("Server unit tests",function(){
         throw err;
       }
       expect(res.status).to.equal(200);
+      done();
+    });
+  });
+
+
+  it("Should notify new score is lower than high score",function(done){
+    var scores = {
+      "username": "Juice",
+      "song": "dog",
+      "points": "5",
+      "difficulty": "Hard"
+    }
+
+    server
+    .post('/api/player/score')
+    .send(JSON.stringify(scores))
+    .set('Content-type', 'application/json')
+    .end(function(err,res){
+      if (err) {
+        throw err;
+      }
+      expect(res.status).to.equal(200);
+      done();
+    });
+  });
+
+  it("Should add new high score",function(done){
+    var scores = {
+      "username": "Juice",
+      "song": "dog",
+      "points": "50",
+      "difficulty": "Hard"
+    }
+
+    server
+    .post('/api/player/score')
+    .send(JSON.stringify(scores))
+    .set('Content-type', 'application/json')
+    .end(function(err,res){
+      if (err) {
+        throw err;
+      }
+      expect(res.status).to.equal(200);
+      done();
+    });
+  });
+
+
+  it("should return Error 404 for wrong url links",function(done){
+    server
+    .get("/random")
+    .expect(404)
+    .end(function(err,res){
+      expect(res.status).to.equal(404);
       done();
     });
   });
