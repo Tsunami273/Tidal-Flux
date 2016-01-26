d0dbf9e7130bbc8138d42b815ce6c913656d204c
quadstar <icberg27@gmail.com>
2016-01-26 11:49:31 -0600

    removed text block.

--- client/components/credits/index.js
+++ client/components/credits/index.js
@@ -1,21 +1,3 @@
-"Clion & Kamisory - Reflection (Sanaas Remix) by Sanaas is licensed under a  Creative Commons License."
-"https://soundcloud.com/sanaas/clion-kamisory-reflection-sanaas-remix"
-"https://soundcloud.com/sanaas"
-
-"rekanan - REFLECTION feat. アンテナガール(Yunomi Remix) by yuzame-label is licensed under a  Creative Commons License."
-"https://soundcloud.com/yuzame-label/reflection-yunomi"
-"https://soundcloud.com/tkrism"
-"https://soundcloud.com/rekanan"
-"https://soundcloud.com/yuzame-label"
-
-"Sanaas - Quickdraw by Sanaas is licensed under a  Creative Commons License."
-"https://soundcloud.com/sanaas/sanaas-quickdrawfc-vivid-hardcore"
-"https://soundcloud.com/sanaas"
-
-
-"Rosarium feat 瑤山百霊 by himmel.LEMiao is licensed under a  Creative Commons License."
-"https://soundcloud.com/himmeltengoku/rosarium-feat-f-c-yonder-voice"
-"https://soundcloud.com/himmeltengoku"
 var CreditBlock = require('./creditBlock.js');
 
 Credits = React.createClass({
