d9e20cf3e6531f54972172644f1fe52daada1780
Michael Wheatley <michaelvwheatley@gmail.com>
2016-01-27 14:52:36 -0600

    no fail option set to default of false

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -20,7 +20,7 @@ const main = function(state, action){
     scrollSpeed: 2,
     durations: [4000,3000,2000,1500,1100,750],
     globalOffset: -30,
-    noFail: true
+    noFail: false
   }; 
   switch(action.type){
     case 'NAVIGATE':
