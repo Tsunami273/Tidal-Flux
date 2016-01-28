abdad675c1836dc3b8bdfdbfc8ab0b4747754a99
Festus Kiprop <fyangani@gmail.com>
2016-01-28 12:21:39 -0600

    Merging

--- client/components/login/index.js
+++ client/components/login/index.js
@@ -50,8 +50,8 @@ Login = React.createClass({
           window.localStorage.setItem('username', data.username); 
           window.localStorage.setItem('token', data.token); 
           window.localStorage.setItem('offset', data.offset); 
-          window.localStorage.setItem('keybinds', data.keybinds); 
-      
+          window.localStorage.setItem('keybinds', keybinds); 
+          
           store.dispatch( { type:'SIGN_IN', username : data.username, token : data.token } );
           store.dispatch( setOffset(data.offset) );
           store.dispatch( setKeyBinds(data.keybinds) );
