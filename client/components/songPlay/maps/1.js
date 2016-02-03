41080e50ec07bb557e0ea0208c2b33fda51a24ca
quadstar <icberg27@gmail.com>
2016-02-02 19:32:31 -0600

    changed reflection easy beatmap to accomodate noobs.

--- client/components/songPlay/maps/1.js
+++ client/components/songPlay/maps/1.js
@@ -7,64 +7,64 @@ var Easy = [
 {notes: [0,0,0,0], startTime: 0},
 {notes: [0,0,0,0], startTime: 0}, 
 {notes: [0,0,0,0], startTime: 0}, 
-{notes: [4,0,8,0], startTime: 0}, // 8
-{notes: [32,0,1,0], startTime: 0},
-{notes: [2,0,16,0], startTime: 0}, 
-{notes: [32,0,16,0], startTime: 0}, 
-{notes: [2,0,1,0], startTime: 0}, // 12
-{notes: [4,0,8,0], startTime: 0},
-{notes: [1,0,8,0], startTime: 0}, 
-{notes: [4,0,32,0], startTime: 0}, 
-{notes: [32,16,8,4], startTime: 0}, // 16
-{notes: [2,1,2,4], startTime: 0},
-{notes: [8,16,32,1], startTime: 0}, 
-{notes: [32,8,4,1], startTime: 0}, 
-{notes: [32,0,16,0], startTime: 0}, // 20
-{notes: [8,0,16,0], startTime: 0},
-{notes: [1,0,2,0], startTime: 0}, 
-{notes: [4,0,2,0], startTime: 0}, 
-{notes: [32,0,1,0], startTime: 0}, // 24
-{notes: [8,0,4,0], startTime: 0},
-{notes: [16,0,2,0], startTime: 0}, 
-{notes: [1,0,4,0], startTime: 0}, 
-{notes: [32,8,4,1], startTime: 0}, // 28
-{notes: [8,4,8,4], startTime: 0},
+{notes: [4,0,0,0], startTime: 0}, // 8
+{notes: [32,0,0,0], startTime: 0},
+{notes: [2,0,0,0], startTime: 0}, 
+{notes: [32,0,0,0], startTime: 0}, 
+{notes: [2,0,0,0], startTime: 0}, // 12
+{notes: [4,0,0,0], startTime: 0},
+{notes: [1,0,0,0], startTime: 0}, 
+{notes: [4,0,0,0], startTime: 0}, 
+{notes: [32,0,8,0], startTime: 0}, // 16
+{notes: [2,0,2,0], startTime: 0},
+{notes: [8,0,32,0], startTime: 0}, 
 {notes: [32,0,4,0], startTime: 0}, 
-{notes: [8,0,1,0], startTime: 0}, 
-{notes: [4,0,2,0], startTime: 0}, // 32
-{notes: [16,0,2,0], startTime: 0},
-{notes: [32,0,2,0], startTime: 0}, 
-{notes: [1,0,8,0], startTime: 0}, 
-{notes: [32,1,16,2], startTime: 0}, //36
-{notes: [8,4,8,4], startTime: 0},
-{notes: [32,0,1,0], startTime: 0}, 
-{notes: [8,0,16,0], startTime: 0}, 
-{notes: [4,0,2,0], startTime: 0}, //40
-{notes: [1,0,8,0], startTime: 0},
-{notes: [16,0,32,0], startTime: 0}, 
-{notes: [8,0,1,0], startTime: 0}, 
-{notes: [2,0,4,0], startTime: 0}, //44 
-{notes: [1,0,32,0], startTime: 0},
-{notes: [8,0,2,0], startTime: 0}, 
-{notes: [16,0,4,0], startTime: 0}, 
-{notes: [32,16,8,4], startTime: 0}, // 48
-{notes: [2,1,2,4], startTime: 0},
-{notes: [8,16,32,1], startTime: 0}, 
-{notes: [32,8,4,1], startTime: 0}, 
+{notes: [32,0,0,0], startTime: 0}, // 20
+{notes: [8,0,0,0], startTime: 0},
+{notes: [1,0,0,0], startTime: 0}, 
+{notes: [4,0,0,0], startTime: 0}, 
+{notes: [32,0,0,0], startTime: 0}, // 24
+{notes: [8,0,0,0], startTime: 0},
+{notes: [16,0,0,0], startTime: 0}, 
+{notes: [1,0,0,0], startTime: 0}, 
+{notes: [32,0,4,0], startTime: 0}, // 28
+{notes: [8,0,8,0], startTime: 0},
+{notes: [32,0,0,0], startTime: 0}, 
+{notes: [8,0,0,0], startTime: 0}, 
+{notes: [4,0,0,0], startTime: 0}, // 32
+{notes: [16,0,0,0], startTime: 0},
+{notes: [32,0,0,0], startTime: 0}, 
+{notes: [1,0,0,0], startTime: 0}, 
+{notes: [32,0,16,0], startTime: 0}, //36
+{notes: [8,0,8,0], startTime: 0},
+{notes: [32,0,0,0], startTime: 0}, 
+{notes: [8,0,0,0], startTime: 0}, 
+{notes: [4,0,0,0], startTime: 0}, //40
+{notes: [1,0,0,0], startTime: 0},
+{notes: [16,0,0,0], startTime: 0}, 
+{notes: [8,0,0,0], startTime: 0}, 
+{notes: [2,0,0,0], startTime: 0}, //44 
+{notes: [1,0,0,0], startTime: 0},
+{notes: [8,0,0,0], startTime: 0}, 
+{notes: [16,0,0,0], startTime: 0}, 
+{notes: [32,0,8,0], startTime: 0}, // 48
+{notes: [2,0,2,0], startTime: 0},
+{notes: [8,0,32,0], startTime: 0}, 
+{notes: [32,0,4,0], startTime: 0}, 
 {notes: [32,0,16,0], startTime: 0}, // 52
-{notes: [8,0,16,0], startTime: 0},
-{notes: [1,0,2,0], startTime: 0}, 
-{notes: [4,0,2,0], startTime: 0}, 
-{notes: [32,0,1,0], startTime: 0}, // 56
-{notes: [8,0,4,0], startTime: 0},
-{notes: [16,0,2,0], startTime: 0}, 
-{notes: [1,0,4,0], startTime: 0}, 
-{notes: [32,8,4,1], startTime: 0}, // 60
-{notes: [8,4,8,4], startTime: 0},
-{notes: [32,0,4,0], startTime: 0}, 
-{notes: [8,0,1,0], startTime: 0}, 
-{notes: [4,0,2,0], startTime: 0}, // 64
-{notes: [8,0,1,0], startTime: 0}, 
+{notes: [8,0,0,0], startTime: 0},
+{notes: [1,0,0,0], startTime: 0}, 
+{notes: [4,0,0,0], startTime: 0}, 
+{notes: [32,0,0,0], startTime: 0}, // 56
+{notes: [8,0,0,0], startTime: 0},
+{notes: [16,0,0,0], startTime: 0}, 
+{notes: [1,0,0,0], startTime: 0}, 
+{notes: [32,0,4,0], startTime: 0}, // 60
+{notes: [8,0,8,0], startTime: 0},
+{notes: [32,0,0,0], startTime: 0}, 
+{notes: [8,0,0,0], startTime: 0}, 
+{notes: [4,0,0,0], startTime: 0}, // 64
+{notes: [8,0,0,0], startTime: 0}, 
 
 ]; 
 var Medium = [
