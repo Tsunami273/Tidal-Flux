4d13092d25aa8e3e9c190c5130a82959a6f663bd
Ian Berg <icberg27@gmail.com>
2024-07-31 17:44:40 -0500

    added audio files

--- server/mongodb.js
+++ server/mongodb.js
@@ -13,19 +13,19 @@ var Schema = mongoose.Schema;
 
  
 var PlayerSchema = new Schema({
-	username   : { type: String, required: true, unique: true },
-	email      : { type: String, required: true, unique: true },
-	password   : { type: String, required: true },
-	keybinds   : [],
- 	offset     : Number
+  username   : { type: String, required: true, unique: true },
+  email      : { type: String, required: true, unique: true },
+  password   : { type: String, required: true },
+  keybinds   : [],
+  offset     : Number
 })
 
 var ScoresSchema = new Schema({
-	username   : String,
-	songId     : Number,
-	points     : Number,
-	difficulty : String,
-	hits       : []
+  username   : String,
+  songId     : Number,
+  points     : Number,
+  difficulty : String,
+  hits       : []
 })
 
 module.exports.Player = mongoose.model('Player', PlayerSchema);
